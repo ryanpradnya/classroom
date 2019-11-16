9557436e3b9aa86d6994ce0acd0ec729514b7caa
ryanpradnya <ryanpradnya@gmail.com>
2019-11-16 22:31:38 +0700

    Adding user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+    const user = sequelize.define('user', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: Sequelize.STRING,
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+
+    return user;
+}
